6a821ac84304b808855548b0ff65b1fc35d3185c
phil <see78cin@gmail.com>
2022-08-28 17:20:17 -0700

    Push 5

--- headtails.py
+++ headtails.py
@@ -1,16 +1,15 @@
 import random
 
 numberOfStreaks = 0  # keep track of streaks
-TnumberOfStreaks = 0  # keep track of Tail streak
-HnumberOfStreaks = 0  # keep track of Head streak
 temp = list()
-
-for trials in range(2):
+trials = int(input('Enter number of trials: '))
+coinflips = int(input('Enter number of trials: '))
+for trial in range(trials):
 
     tail = 0  # keep track of tail count
     head = 0  # keep track of head count
     outcome = list() # list of outcomes
-    for coinflip in range(100):
+    for coinflip in range(coinflips):
         ht = random.randint(0, 1) # tail is 1 , head is 0
         # print(ht)
         if ht == 1:
@@ -24,20 +23,17 @@ for trials in range(2):
         else:
             head = 0  # there is no head, so reset head count to 0
         if tail == 6:
-            TnumberOfStreaks += 1 # there is 6 tails, so increase streak count
+            numberOfStreaks += 1 # there is 6 tails, so increase streak count
         if head == 6:
-            HnumberOfStreaks += 1 # there is 6 head, so increase streak count
+            numberOfStreaks += 1 # there is 6 head, so increase streak count
     temp.append(outcome)
-numberOfStreaks = TnumberOfStreaks+HnumberOfStreaks
-print(f'{trials+1} trials of {coinflip+1} coin flips')
+
+print(f'{trials} trials of {coinflips} coin flips')
 #print(outcome, len(outcome))
-print(f'Tail streak {TnumberOfStreaks}')
-print(f'Head streak {HnumberOfStreaks}')
 print(f'Total streak {numberOfStreaks}')
 format_numOfStreaks = format(numberOfStreaks/10000,'.5f')
 print(f'Chance of streak: {format_numOfStreaks}')
-print('List of trials')
-print(temp)
+
 
 
 
